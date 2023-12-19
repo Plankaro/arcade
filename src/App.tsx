6cb1ae619bcf5626435be2761bbc1952eb82b5cf
Rabin karmakar <106877477+rabin97@users.noreply.github.com>
2023-12-19 16:28:27 +0530

    change style

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,26 @@ import { useMediaQuery } from "react-responsive";
 
 function App() {
   const isMobile = useMediaQuery({ maxHeight: 767 });
-
+  //   const styles = {
+  //     margin: 0;
+  //     padding: 0;
+  //     background- image: url("./assets/home/home.jpg"); /* Adjust the path accordingly */
+  //   background - size: cover;
+  //   background - position: center;
+  //   background - repeat: no - repeat;
+  // }
   return (
-    <div className="w-full h-full p-2 flex flex-col">
-      <div className="w-full h-[5rem] p-3 relative ">
-        <Logo />
-      </div>
-      {/* <CommonButton /> */}
-      <div className={`  flex-grow ${isMobile ? "mt-3" : "mt-5"}`}>
-        <AllOptions />
+    <div className="w-screen h-screen">
+
+
+      <div className="main w-full h-full p-2 flex flex-col" >
+        <div className="w-full h-[5rem] p-3 relative ">
+          <Logo />
+        </div>
+        {/* <CommonButton /> */}
+        <div className={`  flex-grow ${isMobile ? "mt-3" : "mt-5"}`}>
+          <AllOptions />
+        </div>
       </div>
     </div>
   );
