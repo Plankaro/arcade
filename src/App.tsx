9a3cad2c462d0b716b0f32c5ece15f6f23417541
Rabin karmakar <106877477+rabin97@users.noreply.github.com>
2023-12-21 13:23:45 +0530

    add tensition screen in 3d

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import { allClose, openFullScreen } from "./store/slice/action";
 // import Sample from "./components/PdfModel";
 // import { toggleFullScreenAction } from 'path/to/your/actions'; // Import your action
-// import RoommComponent from "./components/RoomComponent";
+import RoommComponent from "./components/RoomComponent";
 import IntroModal from "./components/transitions/IntroModal";
 import PalladianTour from "./components/transitions/PalladianTour";
 import HolisticEcosystem from "./components/transitions/HolisticEcosystem";
 import PdfViewerComponent from "./components/PdfModel";
-import pdf  from "./assets/Arcade_residentail_brochure_R2_compressed (1).pdf"
+import pdf from "./assets/Arcade_residentail_brochure_R2_compressed (1).pdf"
 
 function App() {
   const isMobile = useMediaQuery({ maxHeight: 767 });
@@ -37,22 +37,24 @@ function App() {
   return (
     <FullScreen handle={handle}>
       <div className={`w-screen ${isMobileHeight ? "h-screen" : "h-full"}`}>
+
+        <RoommComponent imageUrl={"/3d.jpg"} nextRoomImageUrl={"/bedroom1-1.jpg"} />
+
         <div className="main relative w-full h-full p-2 flex flex-col">
           <div className="absolute inset-0 z-0 backdrop-brightness-50 " />
           <div className="z-10 w-full h-[5rem] p-3 relative">
             <Logo />
           </div>
-          <div className={`z-10 flex-grow ${isMobile ? "mt-3" : "mt-5"}`}>
+          <div className={`z-10 px-[1rem] flex-grow ${isMobile ? "mt-3" : "mt-5"}`}>
             <AllOptions toggleFullScreen={toggleFullScreen} />
           </div>
           <div className="z-10 w-full h-[5rem] p-3 relative">
-            <IntroModal />
+            {/* <IntroModal />
             <PalladianTour />
             <HolisticEcosystem />
-            <PdfViewerComponent  document={pdf}/>
+            <PdfViewerComponent document={pdf} /> */}
           </div>
         </div>
-        {/* <RoommComponent imageUrl={"https://images.unsplash.com/photo-1625015531264-43c3fce8c792?q=80&w=1460&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} /> */}
       </div>
     </FullScreen>
   );
--- src/components/options/AllOptions.tsx
+++ src/components/options/AllOptions.tsx
@@ -14,7 +14,7 @@ import { IoIosContacts } from "react-icons/io";
 import { BsFullscreen } from "react-icons/bs";
 
 import { useDispatch } from 'react-redux';
-import { open3dpalladian, openHolisticsEcoststem, openIntroVideo, openSalesPresenter } from '../../store/slice/action';
+import { open3dHomeTour, open3dpalladian, openHolisticsEcoststem, openIntroVideo, openSalesPresenter } from '../../store/slice/action';
 
 interface AllOptionsProps{
   toggleFullScreen:()=>void
@@ -37,7 +37,7 @@ const AllOptions:React.FC<AllOptionsProps> = ({
                 <div><CommonButton onClick={() => {}} label='Plans' icon={FaRegMap}/></div>
             </div>
             <div className="flex flex-col gap-4 justify-between h-full">
-                <div><CommonButton onClick={() => {}} right label='3D Home Tour' icon={BsHeadsetVr}/></div>
+                <div><CommonButton onClick={() => {dispatch(open3dHomeTour())}} right label='3D Home Tour' icon={BsHeadsetVr}/></div>
                 <div><CommonButton onClick={() => {dispatch(openSalesPresenter())}} right label='Sales Presenter' icon={RiPresentationLine}/></div>
                 <div><CommonButton onClick={() => {}} right label='Gallery' icon={GrGallery}/></div>
                 <div><CommonButton onClick={() => {}} right label='Contact Us' icon={LuContact}/></div>
