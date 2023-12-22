1c8c732d785cf220ce7198d6176477302a4768e3
Avinash <61863628+Avinash-sord12k@users.noreply.github.com>
2023-12-22 09:22:38 +0530

    added full page loader and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,26 @@ import { allClose, openFullScreen } from "./store/slice/action";
 // import Sample from "./components/PdfModel";
 // import { toggleFullScreenAction } from 'path/to/your/actions'; // Import your action
 import RoommComponent from "./components/RoomComponent";
-import IntroModal from "./components/transitions/IntroModal";
-import PalladianTour from "./components/transitions/PalladianTour";
-import HolisticEcosystem from "./components/transitions/HolisticEcosystem";
-import PdfViewerComponent from "./components/PdfModel";
 import pdf from "./assets/Arcade_residentail_brochure_R2_compressed (1).pdf"
 import Neighbourhood360View from "./components/transitions/Neighbourhood360View";
-import Plans from "./components/transitions/Plans";
-import Layouts from "./components/transitions/Layouts";
-import GalaryModel from "./components/GalaryModel";
 
-// import FullPageLoading from "./components/extras/FullPageLoading";
-// const IntroModal = React.lazy(() => import("./components/transitions/IntroModal"));
-// const PalladianTour = React.lazy(() => import("./components/transitions/PalladianTour"));
-// const HolisticEcosystem = React.lazy(() => import("./components/transitions/HolisticEcosystem"));
+// import IntroModal from "./components/transitions/IntroModal";
+// import PalladianTour from "./components/transitions/PalladianTour";
+// import HolisticEcosystem from "./components/transitions/HolisticEcosystem";
+// import Plans from "./components/transitions/Plans";
+// import Layouts from "./components/transitions/Layouts";
+// import GalaryModel from "./components/GalaryModel";
+// import PdfViewerComponent from "./components/PdfModel";
+
+import FullPageLoading from "./components/extras/FullPageLoading";
+import React from "react";
+const IntroModal = React.lazy(() => import("./components/transitions/IntroModal"));
+const HolisticEcosystem = React.lazy(() => import("./components/transitions/HolisticEcosystem"));
+const Layouts = React.lazy(() => import("./components/transitions/Layouts"));
+const PalladianTour = React.lazy(() => import("./components/transitions/PalladianTour"));
+const Plans = React.lazy(() => import("./components/transitions/Plans"));
+const GalaryModel = React.lazy(() => import("./components/GalaryModel"));
+const PdfViewerComponent = React.lazy(() => import("./components/PdfModel"));
 
 function App() {
   const isMobile = useMediaQuery({ maxHeight: 767 });
@@ -45,7 +51,7 @@ function App() {
 
   console.log("=> Main app rendered");
 
-  
+
 
   return (
     <FullScreen handle={handle}>
@@ -62,14 +68,28 @@ function App() {
             <AllOptions toggleFullScreen={toggleFullScreen} />
           </div>
           <div className="z-10 w-full p-3 relative">
-            {appSelector?.isIntroVideo && <IntroModal />}
-            {appSelector?.isHolisticsEcoststem && <HolisticEcosystem />}
+            <React.Suspense fallback={<FullPageLoading />}>
+              {appSelector?.isIntroVideo && <IntroModal />}
+            </React.Suspense>
+            <React.Suspense fallback={<FullPageLoading />}>
+              {appSelector?.isHolisticsEcoststem && <HolisticEcosystem />}
+            </React.Suspense>
             {<Neighbourhood360View />}
-            {appSelector?.ispalladian && <Layouts />}
-            {appSelector?.is3dpalladian && <PalladianTour />}
-            {appSelector?.isplans && <Plans />}
-            {appSelector?.isSalesPresenter && <PdfViewerComponent document={pdf} />}
-            {appSelector?.isGalary && <GalaryModel />}
+            <React.Suspense fallback={<FullPageLoading />}>
+              {appSelector?.ispalladian && <Layouts />}
+            </React.Suspense>
+            <React.Suspense fallback={<FullPageLoading />}>
+              {appSelector?.is3dpalladian && <PalladianTour />}
+            </React.Suspense>
+            <React.Suspense fallback={<FullPageLoading />}>
+              {appSelector?.isplans && <Plans />}
+            </React.Suspense>
+            <React.Suspense fallback={<FullPageLoading />}>
+              {appSelector?.isSalesPresenter && <PdfViewerComponent document={pdf} />}
+            </React.Suspense>
+            <React.Suspense fallback={<FullPageLoading />}>
+              {appSelector?.isGalary && <GalaryModel />}
+            </React.Suspense>
           </div>
         </div>
       </div>
--- src/components/extras/FullPageLoading.tsx
+++ src/components/extras/FullPageLoading.tsx
@@ -1,11 +1,21 @@
+import { motion } from 'framer-motion';
+import logo from '../../assets/logo/logo.png';
+
 const FullPageLoading = () => {
   return (
-    <div className=' fixed inset-0 backdrop-brightness-50 flex items-center justify-center z-50'>
-      <div className=' bg-white p-4 rounded-md'>
-        <h1 className=' text-2xl font-bold'>Loading...</h1>
-      </div>
-    </div>
-  )
+    <motion.div className='fixed inset-0 backdrop-brightness-50 flex items-center justify-center z-50'>
+      <motion.div className='bg-white h-40 w-40 rounded-full flex items-center justify-center relative overflow-hidden'>
+        <img
+          src={logo}
+          alt="logo"
+          className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 w-4/5 h-4/5 rounded-full"
+        />
+        <svg className="loading-spinner absolute top-0 left-0 w-full h-full">
+          <circle cx="50%" cy="50%" r="48%" />
+        </svg>
+      </motion.div>
+    </motion.div>
+  );
 }
 
-export default FullPageLoading
+export default FullPageLoading;
