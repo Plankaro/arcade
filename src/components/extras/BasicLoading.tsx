7283c7f46ef5560fd0efc756bff8c4c3b254b06c
Avinash <61863628+Avinash-sord12k@users.noreply.github.com>
2023-12-28 03:39:10 +0530

    Refactor BasicLoading component

--- src/components/extras/BasicLoading.tsx
+++ src/components/extras/BasicLoading.tsx
@@ -4,15 +4,6 @@ import logo from "../../assets/logo/logo.png";
 
 const BasicLoading = () => {
   return (
-    // <motion.div
-    //   className='flex items-center justify-center animate-spin text-5xl text-inherit'
-    //   initial={{ opacity: 0 }}
-    //   animate={{ opacity: 1 }}
-    //   exit={{ opacity: 0 }}
-    //   transition={{ duration: 0.3 }}
-    // >
-    //   <AiOutlineLoading3Quarters />
-    // </motion.div>
     <motion.div
       initial={{
         scale: 0,
@@ -27,11 +18,11 @@ const BasicLoading = () => {
         duration: 0.5,
         ease: "easeInOut",
       }}
-      className='bg-white h-[20dvh] w-[20dvh] rounded-full flex items-center justify-center relative overflow-hidden'>
+      className='bg-black/30 h-[20dvh] w-[20dvh] rounded-full flex items-center justify-center relative overflow-hidden'>
       <img
         src={logo}
         alt="logo"
-        className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 w-4/5 h-4/5 rounded-full"
+        className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 w-3/5 rounded-full"
       />
       <svg className="loading-spinner absolute top-0 left-0 w-full h-full">
         <circle cx="50%" cy="50%" r="48%" />
