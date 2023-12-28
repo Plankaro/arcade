942a32c3079dc3b29dfbece1459f2b5c65a6a61d
Avinash <61863628+Avinash-sord12k@users.noreply.github.com>
2023-12-28 16:02:45 +0530

    floor plan layout same as plan layout

--- src/components/transitions/FloorPlan.tsx
+++ src/components/transitions/FloorPlan.tsx
@@ -9,7 +9,7 @@ import NestedSidebar from '../shared/NestedSidebar';
 
 const Layouts = () => {
   const ispalladian = useSelector((state: any) => state?.ispalladian);
-  const isMenuOpen = useSelector((state: any) => state?.isSidebarOpen);
+  const isSidebarOpen = useSelector((state: any) => state?.isSidebarOpen);
   const [src, setSrc] = useState<string | null>(FloorPlans[0].items[0].image);
 
   return (
@@ -28,7 +28,7 @@ const Layouts = () => {
       />
 
       {/* body */}
-      {<div className=" z-10 relative h-screen flex items-center justify-center ">
+      {/* {<div className=" z-10 relative h-screen flex items-center justify-center ">
         <motion.div
           key={src}
           initial={{
@@ -65,8 +65,25 @@ const Layouts = () => {
           </motion.div>
         </motion.div>
 
-      </div>
-      }
+      </div>} */}
+
+      <motion.div
+        initial={{ translateX: 0 }}
+        animate={{ translateX: isSidebarOpen && window.innerWidth > 1000 ? 100 : 0 }}
+        transition={{ duration: 0.5 }}
+        className="z-10 relative h-screen flex items-center justify-center">
+        <motion.div
+          key={src}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className='absolute max-w-[60dvh] bg-white rounded-md overflow-hidden shadow-2xl flex items-center justify-center'
+        >
+          <Image
+            src={src ?? FloorPlans[0].items[0].image}
+            className={"bg-white max-h-[90dvh] rounded-md"} />
+        </motion.div>
+      </motion.div>
 
       {/* close button */}
       <CloseButton />
