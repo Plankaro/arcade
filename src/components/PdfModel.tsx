a7b57bef097030fb80225ec39e8d12cd0b728141
Rabin karmakar <106877477+rabin97@users.noreply.github.com>
2023-12-28 11:18:49 +0530

    remove condition in pdf

--- src/components/PdfModel.tsx
+++ src/components/PdfModel.tsx
@@ -1,21 +1,21 @@
-import { useCallback, useState } from 'react';
-import { useResizeObserver } from '@wojtekmaj/react-hooks';
-import { pdfjs, Document, Page } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+import { useCallback, useState } from "react";
+import { useResizeObserver } from "@wojtekmaj/react-hooks";
+import { pdfjs, Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 import pdf from "../assets/Arcade_residentail_brochure_R2_compressed (1).pdf";
 
-import type { PDFDocumentProxy } from 'pdfjs-dist';
-import CloseButton from './options/CloseButton';
+import type { PDFDocumentProxy } from "pdfjs-dist";
+import CloseButton from "./options/CloseButton";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
 ).toString();
 
 const options = {
-  cMapUrl: '/cmaps/',
-  standardFontDataUrl: '/standard_fonts/',
+  cMapUrl: "/cmaps/",
+  standardFontDataUrl: "/standard_fonts/",
 };
 
 const resizeObserverOptions = {};
@@ -41,31 +41,38 @@ export default function Sample() {
 
   useResizeObserver(containerRef, resizeObserverOptions, onResize);
 
-  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
+  function onDocumentLoadSuccess({
+    numPages: nextNumPages,
+  }: PDFDocumentProxy): void {
     setNumPages(nextNumPages);
   }
 
   return (
-    <div className='fixed w-screen h-full inset-0 z-30 flex items-start mt-3 justify-center'>
-      <div className='absolute right-9 top-4'>
-      <CloseButton />
-
-      
-
+    <div className="fixed w-screen h-full inset-0 z-30 flex items-start mt-3 justify-center">
+      <div className="absolute right-9 top-4">
+        <CloseButton />
       </div>
-        <div className=' w-full justify-center p-4 overflow-hidden overflow-y-scroll' style={{ height: `calc(100% - 2rem)` }} ref={setContainerRef}>
-          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
-            {Array.from(new Array(numPages), (_, index) => (
-              <Page
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                width={ maxWidth}
-                
-              />
-            ))}
-          </Document>
-        </div>
+      <div
+        className=" w-full justify-center p-4 overflow-hidden overflow-y-scroll"
+        style={{ height: `calc(100% - 2rem)` }}
+        ref={setContainerRef}
+      >
+        <Document
+          file={file}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          {Array.from(new Array(numPages), (_, index) => (
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              width={
+                containerWidth ?  maxWidth : maxWidth
+              }
+            />
+          ))}
+        </Document>
+      </div>
     </div>
-
   );
 }
